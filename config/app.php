e38859e47a363e0fcfd718ab07436510e6e4feda
Enrico Bregoli <bregolienrico@gmail.com>
2023-06-23 13:01:01 +0200

    Setup Laravel Permissions nel progetto

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class, // Laravel Permissions by Spatie
     ])->toArray(),
 
     /*
