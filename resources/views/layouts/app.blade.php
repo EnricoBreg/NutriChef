d2949b5b48cc11d0c4b8bd795e779cf1e9f03c7e
Enrico Bregoli <bregolienrico@gmail.com>
2023-06-23 14:51:04 +0200

    Aggiunto jquery min e uncompressed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,12 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        @if(config('app.debug'))
+            <script src="{{ asset("js/resources/jquery-3.7.0/jquery-3.7.0-uncompressed-dev") }}"></script>
+        @else
+            <script src="{{ asset("js/resources/jquery-3.7.0/jquery-3.7.0.min-prod") }}"></script>
+        @endif
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
