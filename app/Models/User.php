cc1fa2320f71fee0a9233025b9924e633b18b4d6
Enrico <enrico.bregoli@edu.unife.it>
2023-06-25 13:27:08 +0200

    aggiunto esempio di encryptable field per modello User

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use HasRoles;
+// use betterapp\LaravelDbEncrypter\Traits\EncryptableDbAttribute;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    //use EncryptableDbAttribute;
+    
     /**
      * The attributes that are mass assignable.
      *
@@ -42,5 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+    ];
+
+    /** @var array The attributes that should be encrypted/decrypted */
+    protected $encryptable = [
     ];
 }
